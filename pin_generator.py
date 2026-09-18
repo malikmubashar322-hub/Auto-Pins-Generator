f2ad226b1a3883806ca1a427edc606bef62eeb67
malikmubashar322-hub <malikmubashar322@gmail.com>
2026-09-18 18:56:43 +0500

    Update pin_generator.py

--- pin_generator.py
+++ pin_generator.py
@@ -512,7 +512,6 @@ def main():
     print(f"Fetching sheet: {SHEET_CSV_URL}")
     df = pd.read_csv(SHEET_CSV_URL, engine='python', on_bad_lines='skip', dtype=str, keep_default_na=False)
     df.columns = [c.strip().lower() for c in df.columns]
-    # url link ko url me badlo
     if "url link" in df.columns:
         df = df.rename(columns={"url link": "url"})
       
